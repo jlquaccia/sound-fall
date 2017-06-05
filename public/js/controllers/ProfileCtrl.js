9ab887bfa7bd80d9c9da29fe0392c85217100217
Jason Quaccia <jlquaccia@gmail.com>
2017-06-05 14:03:38 -0700

    setup post form overlay

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -2,6 +2,7 @@
   angular
     .module('sound_fall')
     .controller('ProfileCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
-      
+      $scope.overlayOpen = false;
+      $scope.errorMessage = null;
     }]);
 })();
