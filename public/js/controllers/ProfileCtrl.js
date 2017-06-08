48ce7e0cafa9e5c83b162dbc65130a24f60bf3c4
Jason Quaccia <jlquaccia@gmail.com>
2017-06-07 21:45:05 -0700

    progress on tag functionality with new post form

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -4,5 +4,18 @@
     .controller('ProfileCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
       $scope.overlayOpen = false;
       $scope.errorMessage = null;
+      
+      $scope.post = {
+        title: '',
+        artist: '',
+        tags: []
+      };
+
+      $scope.addTag = function () {
+        var tag = document.getElementsByName('tags')[0].value;
+
+        $scope.post.tags.push(tag);
+        document.getElementsByName('tags')[0].value = '';
+      };
     }]);
 })();
